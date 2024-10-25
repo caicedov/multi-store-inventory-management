89e9db25a8424fda3e9b7b6ed4e8d2eb10630257
Victor Caicedo <victorcaiicedo@gmail.com>
2024-10-25 01:54:38 -0300

    chore: setup project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<h1 className="text-4xl font-bold">Multi-Store Inventory Management</h1>
+			<p className="text-xl">
+				A modern web application for managing inventory across multiple store
+				locations.
+			</p>
+		</div>
+	);
+}
